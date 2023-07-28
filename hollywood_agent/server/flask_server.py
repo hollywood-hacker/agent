59df75a6fd7ce0ff8252f803c4862b139baeeaf3
Ryan Flynn <ryan@narwh.al>
2023-07-28 16:24:06 -0700

    upload and open

--- hollywood_agent/server/flask_server.py
+++ hollywood_agent/server/flask_server.py
@@ -1,6 +1,9 @@
 # flask_server.py
 from flask import Flask, request
+from werkzeug.utils import secure_filename
 import keyboard
+import os
+import subprocess
 
 class FlaskServer:
     """Class to encapsulate the Flask server."""
@@ -31,4 +34,30 @@ class FlaskServer:
             else:
                 return "No text provided."
         
+        @self.app.route('/upload', methods=['POST'])
+        def file_upload():
+            if 'file' not in request.files:
+                return 'No file part in the request', 400
+
+            file = request.files['file']
+
+            if file.filename == '':
+                return 'No file selected for uploading', 400
+
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            UPLOAD_FOLDER = os.path.join(script_dir, 'uploaded_assets')
+
+            # Check if the upload directory exists, create it if it doesn't
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(filepath)
+
+            # Open the file using the system's default program
+            subprocess.run(['open', filepath])
+
+            return f'File {filename} uploaded and opened successfully', 200
+
         self.app.run(host=self.server_ip, port=self.server_port)
